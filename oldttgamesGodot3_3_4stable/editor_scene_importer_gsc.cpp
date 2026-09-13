d7723b53ffeff266a6d68fe9c10e780d2786e0d2
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2026-09-13 15:14:19 +0100

    todo get other pallete offset todo and todo read MS00

--- oldttgamesGodot3_3_4stable/editor_scene_importer_gsc.cpp
+++ oldttgamesGodot3_3_4stable/editor_scene_importer_gsc.cpp
@@ -27,7 +27,7 @@ Error EditorSceneImporterGSC::import_scene(String &p_path) {
 			uint32_t primtype = f->get_32();
 			if (primtype == int(6)){
 				uint32_t padding01 = f->get_32();
-			} else if (primtype != int(6)){
+			} else {
 				uint32_t primtype_ = f->get_32();
 				uint32_t padding01_ = f->get_32();
 			}
@@ -37,7 +37,7 @@ Error EditorSceneImporterGSC::import_scene(String &p_path) {
 			if (NTBL_FileSize == int(16)){
 				uint32_t namedtablelength = f->get_32();
 				uint32_t padding02 = f->get_32();
-			} else if (NTBL_FileSize != (16)){
+			} else {
 				uint32_t namedtablelength_ = f->get_32();
 			}
 		} else if (Chunk == int(810832724)){
@@ -79,17 +79,194 @@ Error EditorSceneImporterGSC::import_scene(String &p_path) {
 						uint8_t type5 = f->get_8();
 						uint8_t value01 = f->get_8();
 						uint16_t null04 = f->get_16();
+						uint32_t pitch01 = f->get_32();
+						uint32_t flag_1 = f->get_32(); //0x0E
+						uint32_t zero01 = f->get_32();
+						uint32_t zero02 = f->get_32();
+						uint32_t zero03 = f->get_32();
+						uint32_t flag_2 = f->get_32(); //0x51
+						uint32_t zero04 = f->get_32();
+						uint32_t comprHeight = f->get_32();
+						uint32_t comprWidth = f->get_32();
+						uint32_t flag_3 = f->get_32(); //0x52
+						uint32_t zero05 = f->get_32();
+						uint32_t zero06 = f->get_32();
+						uint32_t zero07 = f->get_32();
+						uint32_t flag_4 = f->get_32(); //0x53
+						uint32_t zero08 = f->get_32();
+						uint16_t pallete_offset = f->get_16();
+						uint16_t zero09 = f->get_16();
+						uint16_t zero10 = f->get_16();
+						uint16_t pallete_length = f->get_16(); // 2048
+						uint16_t zero11 = f->get_16();
+						uint16_t zero12 = f->get_16();
+						uint16_t zero13 = f->get_16();
+						uint16_t zero14 = f->get_16();
 					} else if (type4 != int(0)){
 						uint32_t padsize01 = f->get_32();
+						for (int i = 0; i<padsize01; ++i){
+							uint8_t padbytes1 = f->get_8();
+						}
+						uint8_t textureRumble = f->get_8();
+						uint8_t textureBrightness = f->get_8();
+						uint8_t null01 = f->get_8();
+						uint8_t flag5 = f->get_8();
+						uint32_t null02 = f->get_32();
+						uint32_t null03 = f->get_32();
+						uint8_t textureRumbleAgain = f->get_8();
+						uint8_t textureBrightnessAgain = f->get_8();
+						uint8_t null01Again = f->get_8();
+						uint8_t flag5Again = f->get_8();
+						uint8_t type5 = f->get_8();
+						uint8_t value01 = f->get_8();
+						uint16_t null04 = f->get_16();
+						uint32_t pitch01 = f->get_32();
+						uint32_t flag_1 = f->get_32(); //0x0E
+						uint32_t zero01 = f->get_32();
+						uint32_t zero02 = f->get_32();
+						uint32_t zero03 = f->get_32();
+						uint32_t flag_2 = f->get_32(); //0x51
+						uint32_t zero04 = f->get_32();
+						uint32_t comprHeight = f->get_32();
+						uint32_t comprWidth = f->get_32();
+						uint32_t flag_3 = f->get_32(); //0x52
+						uint32_t zero05 = f->get_32();
+						uint32_t zero06 = f->get_32();
+						uint32_t zero07 = f->get_32();
+						uint32_t flag_4 = f->get_32(); //0x53
+						uint32_t zero08 = f->get_32();
+						uint16_t pallete_offset = f->get_16();
+						uint16_t zero09 = f->get_16();
+						uint16_t zero10 = f->get_16();
+						uint16_t pallete_length = f->get_16(); // 2048
+						uint16_t zero11 = f->get_16();
+						uint16_t zero12 = f->get_16();
+						uint16_t zero13 = f->get_16();
+						uint16_t zero14 = f->get_16();
+						if (pallete_offset == int(49152)){
+							
+						} else if (pallete_offset == int(40960)){
+							
+						} else if (pallete_offset == int(36864)){
+							
+						} else if (pallete_offset == int(33792)){
+							
+						}
 					}
 				} else if (TexturePaddingSize1 == int(0)){
 					uint32_t pad_01 = f->get_32();
 					uint32_t pad_02 = f->get_32();
+					uint16_t height_ = f->get_16();
+					uint16_t type1 = f->get_16();
+					uint16_t width_ = f->get_16();
+					uint16_t type2 = f->get_16();
+					uint32_t pitch = f->get_32();
+					uint8_t flag1 = f->get_8();
+					uint8_t flag2 = f->get_8();
+					uint8_t flag3 = f->get_8();
+					uint8_t flag4 = f->get_8();
+					uint32_t size1 = f->get_32();
+					uint32_t size2 = f->get_32();
+					uint32_t type3 = f->get_32();
+					uint32_t type4 = f->get_32();
+					if (type4 == int(0)){
+						uint8_t textureRumble = f->get_8();
+						uint8_t textureBrightness = f->get_8();
+						uint8_t null01 = f->get_8();
+						uint8_t flag5 = f->get_8();
+						uint32_t null02 = f->get_32();
+						uint32_t null03 = f->get_32();
+						uint8_t textureRumbleAgain = f->get_8();
+						uint8_t textureBrightnessAgain = f->get_8();
+						uint8_t null01Again = f->get_8();
+						uint8_t flag5Again = f->get_8();
+						uint8_t type5 = f->get_8();
+						uint8_t value01 = f->get_8();
+						uint16_t null04 = f->get_16();
+						uint32_t pitch01 = f->get_32();
+						uint32_t flag_1 = f->get_32(); //0x0E
+						uint32_t zero01 = f->get_32();
+						uint32_t zero02 = f->get_32();
+						uint32_t zero03 = f->get_32();
+						uint32_t flag_2 = f->get_32(); //0x51
+						uint32_t zero04 = f->get_32();
+						uint32_t comprHeight = f->get_32();
+						uint32_t comprWidth = f->get_32();
+						uint32_t flag_3 = f->get_32(); //0x52
+						uint32_t zero05 = f->get_32();
+						uint32_t zero06 = f->get_32();
+						uint32_t zero07 = f->get_32();
+						uint32_t flag_4 = f->get_32(); //0x53
+						uint32_t zero08 = f->get_32();
+						uint16_t pallete_offset = f->get_16();
+						uint16_t zero09 = f->get_16();
+						uint16_t zero10 = f->get_16();
+						uint16_t pallete_length = f->get_16(); // 2048
+						uint16_t zero11 = f->get_16();
+						uint16_t zero12 = f->get_16();
+						uint16_t zero13 = f->get_16();
+						uint16_t zero14 = f->get_16();
+					} else if (type4 != int(0)){
+						uint32_t padsize01 = f->get_32();
+						for (int i = 0; i<padsize01; ++i){
+							uint8_t padbytes1 = f->get_8();
+						}
+						uint8_t textureRumble = f->get_8();
+						uint8_t textureBrightness = f->get_8();
+						uint8_t null01 = f->get_8();
+						uint8_t flag5 = f->get_8();
+						uint32_t null02 = f->get_32();
+						uint32_t null03 = f->get_32();
+						uint8_t textureRumbleAgain = f->get_8();
+						uint8_t textureBrightnessAgain = f->get_8();
+						uint8_t null01Again = f->get_8();
+						uint8_t flag5Again = f->get_8();
+						uint8_t type5 = f->get_8();
+						uint8_t value01 = f->get_8();
+						uint16_t null04 = f->get_16();
+						uint32_t pitch01 = f->get_32();
+						uint32_t flag_1 = f->get_32(); //0x0E
+						uint32_t zero01 = f->get_32();
+						uint32_t zero02 = f->get_32();
+						uint32_t zero03 = f->get_32();
+						uint32_t flag_2 = f->get_32(); //0x51
+						uint32_t zero04 = f->get_32();
+						uint32_t comprHeight = f->get_32();
+						uint32_t comprWidth = f->get_32();
+						uint32_t flag_3 = f->get_32(); //0x52
+						uint32_t zero05 = f->get_32();
+						uint32_t zero06 = f->get_32();
+						uint32_t zero07 = f->get_32();
+						uint32_t flag_4 = f->get_32(); //0x53
+						uint32_t zero08 = f->get_32();
+						uint16_t pallete_offset = f->get_16();
+						uint16_t zero09 = f->get_16();
+						uint16_t zero10 = f->get_16();
+						uint16_t pallete_length = f->get_16(); // 2048
+						uint16_t zero11 = f->get_16();
+						uint16_t zero12 = f->get_16();
+						uint16_t zero13 = f->get_16();
+						uint16_t zero14 = f->get_16();
+						if (pallete_offset == int(49152)){
+							
+						} else if (pallete_offset == int(40960)){
+							
+						} else if (pallete_offset == int(36864)){
+							
+						} else if (pallete_offset == int(33792)){
+							
+						}
+					}
 				
 				}
 				
 			}
 				
+		} else if (Chunk == int(808473421)){
+			//MS00
+			uint32_t MaterialSet_FileSize = f->get_32();
+			uint32_t MaterialCount = f-get_32();
+			uint32_t padding04 = f->get_32();
 		}
 			
 	}
